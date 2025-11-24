fcc6f420c15fef48ba67c3743321d019e97cb4cc
Nelson Lau <nelsonnlauu@gmail.com>
2025-11-25 00:43:58 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,46 +8,40 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
 # ============================================================
-# 1. Ambil argumen dari MLproject
+# 1. Ambil argumen dari MLProject
 # ============================================================
 parser = argparse.ArgumentParser()
-
 parser.add_argument("--data_path", type=str, required=True)
 parser.add_argument("--test_size", type=float, default=0.2)
 parser.add_argument("--random_state", type=int, default=42)
-
 args = parser.parse_args()
 
 # ============================================================
-# 2. Load dataset preprocessing
+# 2. Load Data Preprocessing
 # ============================================================
 df = pd.read_csv(args.data_path)
 
-# Target: rata-rata nilai
+# Target = rata-rata 3 nilai
 df["avg_score"] = df[["math score", "reading score", "writing score"]].mean(axis=1)
 
 X = df.drop(columns=["avg_score"])
 y = df["avg_score"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=args.test_size,
-    random_state=args.random_state
+    X, y, test_size=args.test_size, random_state=args.random_state
 )
 
 # ============================================================
-# 3. Start MLflow run
+# 3. Start run MLflow
 # ============================================================
 mlflow.set_experiment("CI_Workflow_Model")
 
 with mlflow.start_run():
 
-    # Model
     model = RandomForestRegressor(
         n_estimators=100,
         max_depth=5,
-        random_state=args.random_state
+        random_state=42
     )
 
     model.fit(X_train, y_train)
@@ -59,13 +53,12 @@ with mlflow.start_run():
     rmse = np.sqrt(mse)
     r2 = r2_score(y_test, preds)
 
-    # Logging parameters
+    # Logging manual
     mlflow.log_param("n_estimators", 100)
     mlflow.log_param("max_depth", 5)
     mlflow.log_param("test_size", args.test_size)
     mlflow.log_param("random_state", args.random_state)
 
-    # Logging metrics
     mlflow.log_metric("MAE", mae)
     mlflow.log_metric("MSE", mse)
     mlflow.log_metric("RMSE", rmse)
@@ -74,4 +67,4 @@ with mlflow.start_run():
     # Save model
     mlflow.sklearn.log_model(model, "model")
 
-print("Training selesai ✔ R2 =", r2)
+print("Training CI selesai. R2 =", r2)
